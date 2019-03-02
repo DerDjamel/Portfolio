41a91ca43ccf70fec32845f7c749fc17b4d9ff8c
Dermache Djamel <dj.der.13@gmail.com>
2019-03-02 21:55:19 +0100

    adding the landing page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,27 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Service;
 class HomeController extends Controller
 {
+    
     /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
-    /**
-     * Show the application dashboard.
+     * Show the application landing page.
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
     public function index()
     {
-        
-        return view('home');
+        $services = Service::all();
+        return view('home')->with('services', $services);
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,23 +1,66 @@
 @extends('layouts.app')
 
 @section('content')
+<!--  About me Section -->
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+    <section class="about-me">
+        <h4 class="special-title">Introduction</h4>
+        <h1 class="big-title">Design anything.</h1>
+        <p>Hey, I'm Daniel. By now you should already have a good grasp of what I do; so let me share with you a bit about myself first. I'm an open-minded guy with an uncinventional apprpoach to design. I never limit myself to just one creative field - always pushing to design anything and explore new boundaries. I'm constantly evolving, learning, and growing so you can axpect that working with me is going to be a fun and exciting ride. Cool, now that you know me for me, how may I help you today?</p>
+    </section>
+</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
 
-                    You are logged in!
-                </div>
-            </div>
+<!--  Services Section -->
+<div class="container">
+        <section class="services">
+                @foreach ($services as $index => $service)
+                    <article class="service row justify-content-between align-items-center">
+                        
+                        @if ($index % 2 == 0)
+                            <div class="service-content col-md-6">
+                                <div class="number">{{ $index + 1 }}</div>
+                                <h2 class="default-title">{{ $service->name }}</h2>
+                                <p> {{ $service->description }} </p>
+                            </div>
+            
+                            <div class="service-image col-md-6">
+                                <img src="{{ asset($service->image) }}" alt="{{ $service->name }}">
+                            </div>
+                        @else
+                            <div class="service-image col-md-6">
+                                <img src="{{ asset($service->image) }}" alt="{{ $service->name }}">
+                            </div>
+
+                            <div class="service-content col-md-6">
+                                <div class="number">{{ $index }}</div>
+                                <h2 class="default-title">{{ $service->name }}</h2>
+                                <p> {{ $service->description }} </p>
+                            </div>
+                        @endif
+        
+                    </article>
+                @endforeach
+        </section>
+</div>
+
+<div class="container">
+    <section class="contact">
+        <h2 class="big-title">Your Turn.</h2>
+        <p>Get in touch now. Tell me more about yourself and how I can help you</p>
+    </section>
+    
+    <div class="row boxes">
+         <div class="box box1 col-sm-6">
+            <p>Start a project</p>
+            <div class="arrow"><img src="{{ asset('images/arrow.svg') }}" alt="go"></div>
+        </div>
+        <div class="box box2 col-sm-6">
+            <p>Email me</p>
+            <div class="arrow"><img src="{{ asset('images/arrow.svg') }}" alt="go"></div>
         </div>
     </div>
 </div>
+
+
 @endsection
