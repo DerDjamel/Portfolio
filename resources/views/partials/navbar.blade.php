62cdcbd18db0106d2c570ae6e9fbedc1690e01cd
Dermache Djamel <dj.der.13@gmail.com>
2019-03-06 18:37:44 +0100

    add navbar

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+    <div class="container">
+        <a class="navbar-brand" href="{{ url('/') }}">
+            <img src="{{ asset('images/logo.jpg') }}" alt="">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <!-- Right Side Of Navbar -->
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="">Projects</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="">Conatct</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
